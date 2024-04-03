789aee800267d9f3d693ed17864083a1a366c505
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 19:58:18 +0300

    commit

---
+++ scripts/config/config.js
@@ -0,0 +1,28 @@
+// Example config.js
+
+const config = {
+    // Twitter API configuration
+    twitter: {
+        apiKey: 'YOUR_TWITTER_API_KEY',
+        apiSecretKey: 'YOUR_TWITTER_API_SECRET_KEY',
+        accessToken: 'YOUR_TWITTER_ACCESS_TOKEN',
+        accessTokenSecret: 'YOUR_TWITTER_ACCESS_TOKEN_SECRET',
+    },
+
+    // Facebook API configuration
+    facebook: {
+        appId: 'YOUR_FACEBOOK_APP_ID',
+        appSecret: 'YOUR_FACEBOOK_APP_SECRET',
+        accessToken: 'YOUR_FACEBOOK_ACCESS_TOKEN',
+    },
+
+    // Instagram API configuration
+    instagram: {
+        accessToken: 'YOUR_INSTAGRAM_ACCESS_TOKEN',
+    },
+
+    // Other configurations
+    // ...
+};
+
+module.exports = config;
