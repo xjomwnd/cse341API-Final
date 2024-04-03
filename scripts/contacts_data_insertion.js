34f62467cdaad048e8e41934f64648f228c0deea
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 19:46:33 +0300

    commit

---
+++ scripts/contacts_data_insertion.js
@@ -0,0 +1,34 @@
+// Inserting data from the provided JSON into MongoDB collections
+
+// Connect to MongoDB server
+// Make sure MongoDB is running and accessible
+// Use appropriate connection string and database name
+var conn = new Mongo();
+var db = conn.getDB("finalproject");
+
+// Insert contacts data into the contacts collection
+db.contacts.insertMany([
+  {
+    "firstName": "Sarah",
+    "lastName": "Birch",
+    "email": "amazingwoman@test.com",
+    "favoriteColor": "Green",
+    "birthday": "12/12/20"
+  },
+  {
+    "firstName": "Jimmy",
+    "lastName": "Carter",
+    "email": "jCarter@test.com",
+    "favoriteColor": "Purple",
+    "birthday": "07/12/10"
+  },
+  {
+    "firstName": "Cassandra",
+    "lastName": "Anderson",
+    "email": "AndersonC@test.com",
+    "favoriteColor": "Yellow",
+    "birthday": "02/22/95"
+  }
+]);
+
+print("Data inserted successfully.");
