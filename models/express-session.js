8209f07bbb03ea1cbc9a233e449f27b000917353
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-04 16:28:12 +0300

    commit

--- models/express-session.js
+++ models/express-session.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const session = require('express-session'); // Correct import statement
 // eslint-disable-next-line no-unused-vars
-const validateComment = require('./validateComment');
+
 
 // Create a new Express application
 const app = express();
