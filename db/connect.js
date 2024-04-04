0c0bdba53df56ab256d3dd07693a4b98684c081a
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-04 04:18:28 +0300

    commit

--- db/connect.js
+++ db/connect.js
@@ -1,44 +1,31 @@
+Connect.js
 const dotenv = require('dotenv');
 dotenv.config();
+const MongoClient = require('mongodb').MongoClient;
 
-const { MongoClient } = require('mongodb');
 
 let _db;
-
-const initDb = async () => {
-  try {
-    if (_db) {
-      console.log('Db is already initialized!');
-      return _db;
-    }
-    const client = await MongoClient.connect(process.env.MONGODB_URI);
-    _db = client.db(); // Access the database from the client
-    console.log('Connected to MongoDB');
-    return _db;
-  } catch (err) {
-    console.error('Error initializing database:', err);
-    throw err; // Propagate the error to the caller
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
   }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
 };
-
 const getDb = () => {
   if (!_db) {
     throw Error('Db not initialized');
   }
   return _db;
 };
-
-// Close database connection gracefully
-const closeDb = () => {
-  if (_db) {
-    _db.close();
-    _db = null;
-    console.log('Database connection closed');
-  }
-};
-
 module.exports = {
   initDb,
-  getDb,
-  closeDb
+  getDb
 };
