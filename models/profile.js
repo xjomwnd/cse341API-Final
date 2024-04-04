17e546588e06bc5187dca195f13e1d5a7100700c
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-04 14:46:03 +0300

    commiit

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+const profileRouter = require('./profile');
 
 // Define the route handler for the /profile route
 router.get('/', ensureLoggedIn, (req, res) => {
