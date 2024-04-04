e35eb4ed3b300b57765982fcf4002cda645ca7f2
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-04 17:25:36 +0300

    COMMIT

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,93 +1,78 @@
 const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const { ObjectId } = require('mongodb');
 
-const getAll = (req, res) => {
-  mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find()
-    .toArray((err, lists) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
+const getAll = async (req, res) => {
+  try {
+    const contacts = await mongodb.getDb().db().collection('contacts').find().toArray();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-const getSingle = (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to find a contact.');
+const getSingle = async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json('Invalid contact id');
   }
-  const userId = new ObjectId(req.params.id);
-  mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId })
-    .toArray((err, result) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(result[0]);
-    });
+
+  try {
+    const contact = await mongodb.getDb().db().collection('contacts').findOne({ _id: new ObjectId(id) });
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const createContact = async (req, res) => {
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+  const contact = { firstName, lastName, email, favoriteColor, birthday };
+
+  try {
+    const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+    res.status(201).json(response.ops[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
 const updateContact = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to update a contact.');
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json('Invalid contact id');
   }
-  const userId = new ObjectId(req.params.id);
-  // be aware of updateOne if you only want to update specific fields
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .replaceOne({ _id: userId }, contact);
-  console.log(response);
-  if (response.modifiedCount > 0) {
+
+  const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+  const contact = { firstName, lastName, email, favoriteColor, birthday };
+
+  try {
+    const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: new ObjectId(id) }, contact);
+    if (response.modifiedCount === 0) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
     res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
 const deleteContact = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to delete a contact.');
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json('Invalid contact id');
   }
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
+
+  try {
+    const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: new ObjectId(id) });
+    if (response.deletedCount === 0) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
     res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
