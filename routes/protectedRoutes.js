3b02f00e05dc5642d536b4c98f4b01f7ce8c986b
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-04 15:04:38 +0300

    commit

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 // Example route protected with Passport
-app.get('/profile',
+app.get('./profile',
   require('connect-ensure-login').ensureLoggedIn(),
   (req, res) => {
     res.send(`Hello, ${req.user.displayName}`);
