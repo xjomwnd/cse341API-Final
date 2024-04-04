7768b7f2d84fca8bb1f49a2bb64572391b2194e6
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-04 04:11:37 +0300

    COMMIT

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const port = process.env.PORT || 8083;
 const app = express();
 
-require('dotenv').config();
+app.use(bodyParser.json());
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
-
-process.on('uncaughtException', (err, origin) => {
-  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+// Middleware to allow cross-origin requests
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 
+// Routes
+app.use('/', require('./routes'));
 
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+async function startServer() {
+  try {
+    await mongodb.initDb();
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on port ${port}`);
+    });
+  } catch (err) {
+    console.error('Error connecting to database:', err);
+    process.exit(1); // Exit with non-zero code to indicate failure
   }
-});
+}
+
+startServer();
