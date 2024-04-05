f43806b0f9855aa5467fd7d24a1f39de5c2663d9
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-05 10:50:12 +0300

    COMMIT

---
+++ AUTOMETED TESTING/config.js
@@ -0,0 +1,10 @@
+// config.js
+
+// Hardcoded base URL
+export const baseURLHardcoded = 'http://localhost:3000';
+
+// Using environment variable
+export const baseURLEnvVariable = process.env.REACT_APP_API_BASE_URL;
+
+// Dynamically detected base URL
+export const baseURLDynamic = window.location.origin;
