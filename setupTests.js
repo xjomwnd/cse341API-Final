e342092283a8d4a5d52ad9e9e97bf329e0ad39a0
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-05 09:54:29 +0300

    commit

--- setupTests.js
+++
@@ -1,8 +0,0 @@
-// Import testing libraries or setup utilities
-import '@testing-library/jest-dom'; // Example: Jest DOM Matchers for testing library
-import 'jest-extended'; // Example: Jest extended for additional matchers
-
-// Set up global configurations or mocks
-// For example:
-// jest.setTimeout(10000); // Example: Increase Jest timeout for async tests
-// jest.mock('some-module'); // Example: Mock external module for testing
